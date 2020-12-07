2a973b5a895a7b7010b0031215fc0c00fa0b05d5
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-12-07 20:36:37 +0900

    try class & struct

--- CppProjectPractice.cpp
+++ CppProjectPractice.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+struct StructType {
+	
+};
+
 // reference call
 int Test(int& num) {
 	num += 1;
@@ -10,12 +14,43 @@ int Test(int& num) {
 	return num;
 }
 
+class AClass {
+public:
+	int num = 0;
+	string name = "AClass name";
+	struct customStruct {
+		int num = 1;
+		string name = "custom struct name";
+	};
+	union customUnion {
+		int num = 2;
+		string name = "custom union name";
+	};
+	AClass() {
+
+
+	}
+
+	auto returnA() {
+		struct AStruct {
+			int num;
+			string name;
+		};
+		AStruct->num = num;
+		AStruct->name = name;
+		return AStruct;
+	}
+};
+
 
 int main()
 {
 	int num0 = 1;
+	int num1 = 2;
 
 	Test(num0);
+	Test(num1);
+	
 	
 	return 0;
 }
